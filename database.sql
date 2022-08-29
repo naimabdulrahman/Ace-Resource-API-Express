5cbea1c265e9d91057fb81f77d52e28cc5f51bda
Muhammad Na'im <muhammadnaim@Muhammads-MacBook-Pro.local>
2022-08-29 15:14:52 +0800

    Update Database

--- database.sql
+++ database.sql
@@ -2,8 +2,19 @@ CREATE DATABASE my_database;
 
 -- \c into my_database
 
-CREATE TABLE chart(
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    fullname VARCHAR(255)
+    createdAt Date,
+    updatedAt Date
+);
+
+CREATE TABLE charts(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    value NUMBER(255)
+    value NUMBER(255),
+    createdAt Date,
+    updatedAt Date
 )
